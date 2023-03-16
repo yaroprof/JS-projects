50692b75179d4efe42325a30a88b73235bca2a07
yaroprof <crosskyiv@gmail.com>
2023-03-16 16:32:43 +0200

    prouct-filter

--- JS-productFilter/index.js
+++ JS-productFilter/index.js
@@ -1,41 +1,43 @@
-// Get DOM elements
 const categoryFilter = document.getElementById('category');
 const priceFilter = document.getElementById('price-range');
-const filterButton = document.getElementById('filter-btn');
+const filterBtn = document.getElementById('filter-btn');
 const productList = document.getElementById('product-list');
 
+// Map price range values to corresponding filtering conditions
+const priceRanges = {
+  'under-50': price => price < 50,
+  '50-100': price => price > 50 && price < 100,
+  '100-200': price => price > 100 && price < 200,
+  'over-200': price => price >= 200,
+  '': () => true,
+};
+
 // Add event listener to filter button
-filterButton.addEventListener('click', () => {
-  // Get selected filters
+filterBtn.addEventListener('click', () => {
   const selectedCategory = categoryFilter.value;
   const selectedPriceRange = priceFilter.value;
 
-  // Filter products based on selected category and price range
-  const filteredProducts = Array.from(productList.children)
-    .filter(product => {
-      const category = product.getAttribute('data-category');
-      if (selectedCategory !== '' && selectedCategory !== category) {
-        return false;
-      }
+// Get selected filters
 
-      const price = parseFloat(product.getAttribute('data-price'));
-      switch (selectedPriceRange) {
-        case 'under-50':
-          return price < 50;
-        case '50-100':
-          return price >= 50 && price < 100;
-        case '100-200':
-          return price >= 100 && price < 200;
-        case 'over-200':
-          return price >= 200;
-        default:
-          return true;
-      }
-    });
+const filteredProducts = Array.from(productList.children)
+.filter(product => {
+  const category = product.getAttribute('data-category')
+  if (selectedCategory !== '' && selectedCategory !== category) {
+    return false
+  } 
+  const price = parseFloat(product.getAttribute('data-price'))
+  const filterCondition = priceRanges[selectedPriceRange]
+  return filterCondition(price)
+})
 
-  // Display filtered products
-  productList.innerHTML = '';
-  filteredProducts.forEach(product => {
-    productList.appendChild(product);
-  });
+
+// Display filtered products
+  productList.innerHTML = ''
+  filteredProducts.forEach(product =>
+     productList.appendChild(product))
 });
+
+
+
+
+
